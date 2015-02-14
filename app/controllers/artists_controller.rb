6d8fbc29b80cee550f634d33ec5e1e2f8d65745a
William Mahoney <wm.j.mahoney@gmail.com>
2015-02-14 10:32:14 -0500

    modified artists controller to set instance variable @artists

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,7 @@
 class ArtistsController < ApplicationController
   def index
     @opinion = ["Love", "Like", "Don't Know", "Dislike"]
-    @artist = Artist.first(:order => "RANDOM()")
+    # @artist = Artist.first(:order => "RANDOM()")
+    @artists = Artist.all
   end
 end
