7c6cb019a6d0d5cf4777e6cbb2d291732458facc
William Mahoney <wm.j.mahoney@gmail.com>
2015-02-16 07:52:57 -0500

    not using array of opinions, commented out for now

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,6 @@
 class ArtistsController < ApplicationController
   def index
-    @opinion = ["Love", "Like", "Don't Know", "Dislike"]
+    # @opinion = ["Love", "Like", "Don't Know", "Dislike"]
     # @artist = Artist.first(:order => "RANDOM()")
     @artists = Artist.all
   end
