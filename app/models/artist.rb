379492b5d07ebe3885ff8b093a0d10fa2798762b
William Mahoney <wm.j.mahoney@gmail.com>
2015-02-17 11:18:34 -0500

    added class method to gain access to a random unrated(by the current user) artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,14 @@
 class Artist < ActiveRecord::Base
   validates :name, presence: true
+
+  def self.rand_unrated_artist(user)
+    array = []
+    Artist.all.each do |artist|
+      if Taste.find_by(artist_id: artist, user_id: user)
+      else
+        array << artist
+      end
+    end
+    array.sample
+  end
 end
