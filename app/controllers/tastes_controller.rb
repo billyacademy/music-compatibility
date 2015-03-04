b1cce005d57ee9c1f4011c831ad9ac16ac8487b0
William Mahoney <wm.j.mahoney@gmail.com>
2015-03-03 23:16:03 -0500

    added css/scss to artists page, moved flash message to bottom of screen, added a font

--- app/controllers/tastes_controller.rb
+++ app/controllers/tastes_controller.rb
@@ -8,9 +8,18 @@ class TastesController < ApplicationController
     @taste.user_id = params["user_id"]
     @taste.artist_id = params["artist_id"]
     @taste.opinion = params["opinion"]
-    if @taste.save!
+    if @taste.save! && @taste.opinion == "Love"
       redirect_to artists_path,
-      notice: "You have successfully rated an artist!"
+      notice: "You have declared your love for #{Artist.find(@taste.artist_id).name}!"
+    elsif @taste.save! && @taste.opinion == "Like"
+      redirect_to artists_path,
+      notice: "Ohh, I guess you like #{Artist.find(@taste.artist_id).name}!"
+    elsif @taste.save! && @taste.opinion == "Don't Know"
+      redirect_to artists_path,
+      notice: "It's okay, not everyone knows #{Artist.find(@taste.artist_id).name}!"
+    elsif @taste.save! && @taste.opinion == "Dislike"
+      redirect_to artists_path,
+      notice: "#{Artist.find(@taste.artist_id).name} isn't for everyone, we won't tell!"
     else
       render artists_path,
       notice: "Something went wrong, please try again."
