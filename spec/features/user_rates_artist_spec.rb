add61cae59015390a96beaaa8490ab181e4ac4b5
William Mahoney <wm.j.mahoney@gmail.com>
2015-03-05 13:02:36 -0500

    updated tests on rating an artist

--- spec/features/user_rates_artist_spec.rb
+++ spec/features/user_rates_artist_spec.rb
@@ -5,24 +5,40 @@ feature 'user rates an artist', %Q{
   I want rate an artist
   So that I can compare my tastes with other users
   } do
+
     scenario 'rate an artist' do
-      user = FactoryGirl.create(:user)
-      artist = FactoryGirl.create(:artist)
-      sign_in_as(user)
+      @user = FactoryGirl.create(:user)
+      @artist1 = FactoryGirl.create(:artist)
+
+      sign_in_as(@user)
 
       visit artists_path
       click_button "Love"
 
-      expect(page).to have_content "You have successfully rated an artist!"
+      expect(page).to have_content "You have declared your love for"
+
     end
 
-    scenario 'no artist to rate' do
-      user = FactoryGirl.create(:user)
+    scenario 'rate four artists differently' do
+      @user = FactoryGirl.create(:user)
+      @artist1 = FactoryGirl.create(:artist)
+      @artist2 = FactoryGirl.create(:artist)
+      @artist3 = FactoryGirl.create(:artist)
+      @artist4 = FactoryGirl.create(:artist)
 
-      sign_in_as(user)
+      sign_in_as(@user)
 
-      click_link "Rate an Artist"
+      visit artists_path
 
+      click_button "Love"
+      expect(page).to have_content "You have declared your love for"
+      click_button "Like"
+      expect(page).to have_content "Ohh, I guess you like"
+      click_button "Don't Know"
+      expect(page).to have_content "It's okay, not everyone knows"
+      click_button "Dislike"
+      expect(page).to have_content "isn't for everyone, we won't tell!"
       expect(page).to have_content "You have rated every artist"
-    end
+      save_and_open_page
   end
+end
