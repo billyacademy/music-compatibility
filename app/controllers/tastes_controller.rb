cade24f930258f7be3ce93118815f45c9fa79eb9
William Mahoney <wm.j.mahoney@gmail.com>
2015-03-10 17:26:27 -0400

    updated create method so that application wouldn't error out if someone clicks a rate button  too quickly

--- app/controllers/tastes_controller.rb
+++ app/controllers/tastes_controller.rb
@@ -4,24 +4,21 @@ class TastesController < ApplicationController
   end
 
   def create
-    @taste = Taste.new
-    @taste.user_id = params["user_id"]
-    @taste.artist_id = params["artist_id"]
-    @taste.opinion = params["opinion"]
-    if @taste.save! && @taste.opinion == "Love"
+    @taste = Taste.new(taste_params)
+    if @taste.save && @taste.opinion == "Love"
       redirect_to artists_path,
       notice: "You have declared your love for #{Artist.find(@taste.artist_id).name}!"
-    elsif @taste.save! && @taste.opinion == "Like"
+    elsif @taste.save && @taste.opinion == "Like"
       redirect_to artists_path,
       notice: "Ohh, I guess you like #{Artist.find(@taste.artist_id).name}!"
-    elsif @taste.save! && @taste.opinion == "Don't Know"
+    elsif @taste.save && @taste.opinion == "Don't Know"
       redirect_to artists_path,
       notice: "It's okay, not everyone knows #{Artist.find(@taste.artist_id).name}!"
-    elsif @taste.save! && @taste.opinion == "Dislike"
+    elsif @taste.save && @taste.opinion == "Dislike"
       redirect_to artists_path,
       notice: "#{Artist.find(@taste.artist_id).name} isn't for everyone, we won't tell!"
     else
-      render artists_path,
+      redirect_to artists_path,
       notice: "Something went wrong, please try again."
     end
   end
@@ -29,6 +26,6 @@ class TastesController < ApplicationController
   private
 
   def taste_params
-    params.require(:taste).permit(:user_id, :artist_id, :opinion)
+    params.permit(:user_id, :artist_id, :opinion)
   end
 end
--- spec/features/user_rates_artist_spec.rb
+++ spec/features/user_rates_artist_spec.rb
@@ -4,7 +4,7 @@ feature 'user rates an artist', %Q{
   As a user
   I want rate an artist
   So that I can compare my tastes with other users
-  } do
+} do
 
     scenario 'rate an artist' do
       @user = FactoryGirl.create(:user)
@@ -19,25 +19,25 @@ feature 'user rates an artist', %Q{
 
     end
 
-    scenario 'rate four artists differently' do
-      @user = FactoryGirl.create(:user)
-      @artist1 = FactoryGirl.create(:artist)
-      @artist2 = FactoryGirl.create(:artist)
-      @artist3 = FactoryGirl.create(:artist)
-      @artist4 = FactoryGirl.create(:artist)
+  scenario 'rate four artists differently' do
+    @user = FactoryGirl.create(:user)
+    @artist1 = FactoryGirl.create(:artist)
+    @artist2 = FactoryGirl.create(:artist)
+    @artist3 = FactoryGirl.create(:artist)
+    @artist4 = FactoryGirl.create(:artist)
 
-      sign_in_as(@user)
+    sign_in_as(@user)
 
-      visit artists_path
+    visit artists_path
 
-      click_button "Love"
-      expect(page).to have_content "You have declared your love for"
-      click_button "Like"
-      expect(page).to have_content "Ohh, I guess you like"
-      click_button "Don't Know"
-      expect(page).to have_content "It's okay, not everyone knows"
-      click_button "Dislike"
-      expect(page).to have_content "isn't for everyone, we won't tell!"
-      expect(page).to have_content "You have rated every artist"
+    click_button "Love"
+    expect(page).to have_content "You have declared your love for"
+    click_button "Like"
+    expect(page).to have_content "Ohh, I guess you like"
+    click_button "Don't Know"
+    expect(page).to have_content "It's okay, not everyone knows"
+    click_button "Dislike"
+    expect(page).to have_content "isn't for everyone, we won't tell!"
+    expect(page).to have_content "You have rated every artist"
   end
 end
