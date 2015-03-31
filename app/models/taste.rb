24bd38bf392cc7784da627f10c1008d997723508
William Mahoney <wm.j.mahoney@gmail.com>
2015-03-31 17:43:47 -0400

    added some class methods to reduce calls on database

--- app/models/taste.rb
+++ app/models/taste.rb
@@ -8,4 +8,20 @@ class Taste < ActiveRecord::Base
 
   validates :artist_id,
     presence: true
+
+  def self.compare(cur_user, other_user)
+    where(:user_id => [cur_user, other_user])
+  end
+
+  def self.comparison_hash(array)
+    hash = Hash.new
+    array.each do |taste|
+      if hash[taste.artist_id] == nil
+        hash[taste.artist_id] = { taste.user_id => taste.opinion }
+      else
+        hash[taste.artist_id].store(taste.user_id, taste.opinion)
+      end
+    end
+    hash
+  end
 end
